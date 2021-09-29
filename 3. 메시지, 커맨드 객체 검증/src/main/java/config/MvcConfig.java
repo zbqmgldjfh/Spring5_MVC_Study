0bb63af5eb517384b7b19495d63b01df9dfd17da
zbqmgldjfh <60593969+zbqmgldjfh@users.noreply.github.com>
2021-09-29 15:58:59 +0900

    MVC 메시지, 커맨드 객체 검증과정
    커맨드 객체의 값 검증과 에러 메시지 처리를 위한 과정 공부

---
+++ 3. 메시지, 커맨드 객체 검증/src/main/java/config/MvcConfig.java
@@ -0,0 +1,48 @@
+package config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.Validator;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import controller.RegisterRequestValidator;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+	
+	// 글로벌 범위 Validator를 직접 설정하면 OptionalValidatorFactoryBean을 사용하지 않는다.
+//	@Override
+//	public Validator getValidator() {
+//		return new RegisterRequestValidator();
+//	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp("/WEB-INF/view/", ".jsp");
+	}
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/main").setViewName("main");
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
+		ms.setBasename("message.label");
+		ms.setDefaultEncoding("UTF-8");
+		return ms;
+	}
+}
